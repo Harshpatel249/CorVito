0b4c946eb891b00fbf50cee97d85905cc1b75b76
= <=>
2021-03-08 21:03:43 +0530

    card update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cards from '../components/Cards';
 
 export default function Home(props){
     document.body.style = 'background:'+ props.Theme.body;
     return (
         <div>
+        <Cards />
         <h1 style={{ color: props.Theme.text }}>Welcome to the home page!</h1>
         <p style={{ color: props.Theme.text }}>Lorem ipsum dolor</p>
         </div>
